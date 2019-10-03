0fdcb6469845ea2bf8bc4a83f60e35dbbd039642
Aleksa Tesic <tesic@getpenta.com>
2019-10-03 22:52:20 +0200

    Add proper file saving

--- scraper.py
+++ scraper.py
@@ -2,6 +2,9 @@ import scrapy
 
 class GithubNetworkSpider(scrapy.Spider):
     name = 'github_scraper'
+    HTTP_PART = 6
+    HTTPS_PART = 7
+    HTML_EXT = '.html'
     
     def start_requests(self):
         urls = ['http://quotes.toscrape.com/page/1/']
@@ -10,6 +13,27 @@ class GithubNetworkSpider(scrapy.Spider):
             yield scrapy.Request(url = url, callback=self.parse)
 
     def parse(self, response):
-        with open('test.html', 'w') as test:
-            test.write(response.body.decode('utf-8'))
-        self.log('Saved file...')
+        self.save_html_to_file('test.html', response)
+
+    def save_html_to_file(self, response):
+        """
+        Saves .html data got from the response
+        """
+        fname = self.create_file_name_from_url(response.url)
+        print('FNAME: ', fname)
+        with open(fname, 'w') as f:
+            f.write(response.body.decode('utf-8'))
+        self.log('save_html_to_file...')
+    
+    def create_file_name_from_url(self, url):
+        """
+        Receive url and create proper .html file from it
+        """
+        if(url.find('https')):
+            fname = url[self.HTTPS_PART:]
+        else:
+            fname = url[self.HTTP_PART:]
+        fname = fname.replace('/', '-')
+        fname += self.HTML_EXT
+        return fname
+        
