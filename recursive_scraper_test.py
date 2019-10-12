054b91feac15310e4df4ba842a7538be1931fa8c
Aleksa Tesic <tesic@getpenta.com>
2019-10-12 17:29:43 +0200

    Add first recursive test based on regex rules

--- recursive_scraper_test.py
+++ recursive_scraper_test.py
@@ -1,6 +1,6 @@
 import scrapy
 from scrapy.selector import Selector
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy.spiders import CrawlSpider, Rule, Spider
 from scrapy.linkextractors import LinkExtractor
 
 MAX_DEPTH = 4
@@ -12,11 +12,19 @@ class RecursiveGithubNetworkSpider(CrawlSpider):
             'https://github.com/Imafikus?tab=followers',
         ]
     rules = [
-        Rule(LinkExtractor(allow=('(https:\/\/github.com\/)\w+(\?tab=following|\?tab=followers)',)), callback='parse', follow=True),
+    #    Rule(LinkExtractor(allow=('(https:\/\/github.com\/)\w+(\?tab=following|\?tab=followers)',)), callback='parse', follow=True),
+            # Rule(LinkExtractor(allow = r'(https:\/\/github.com\/)\w+(?!\/)'), callback='parse', follow=True),
+            Rule(LinkExtractor(allow = r'(https:\/\/github.com\/)\w+(?!\/)'), callback='parse_item', follow=True),
+        
         ]
 
-    def parse(self, response):
+    def parse_item(self, response):
         print('Parsing...')
         print(response.url)
 
+        """
+            1. extract names from the start_urls
+            2. create links similar to start_urls
+            3. call scrape on them also
+        """
     
